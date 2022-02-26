de732f7569244cb747225047e78e2f8b33d9cb00
Wugawuga <dnr3655@naver.com>
2022-02-26 20:40:20 +0900

    수정 - 장바구니 유저 별로 출력

--- src/main/java/team/kyp/kypcoffee/controller/CartController.java
+++ src/main/java/team/kyp/kypcoffee/controller/CartController.java
@@ -1,10 +1,13 @@
 package team.kyp.kypcoffee.controller;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import team.kyp.kypcoffee.domain.Cart;
 import team.kyp.kypcoffee.domain.CartCommand;
 import team.kyp.kypcoffee.service.CartService;
 import team.kyp.kypcoffee.service.ProductListService;
@@ -23,5 +26,12 @@ public class CartController {
         cartService.createCart(cartCommand);
 
         return "redirect:/product";
+    }
+
+    @GetMapping("/cartList")
+    public String cartList(Session session) {
+
+
+        return null;
     }
 }
