ef5f55416d5a6d66f7b395efb54d4be947aa1a9a
MJguri <pmy20804@gmail.com>
2022-03-04 16:57:12 +0900

    add - OnedayClass Detail

---
+++ src/main/java/team/kyp/kypcoffee/domain/OnedayClassApplierInfo.java
@@ -0,0 +1,21 @@
+package team.kyp.kypcoffee.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class OnedayClassApplierInfo {
+    private int classNum;
+    private Date classDate;
+    private int classCapacity;
+    private int classApplicantsNum;
+    private int memberNum;
+    private String memberName;
+    private String memberAddress;
+    private String memberTel;
+    private String memberPhone;
+    private String memberEamil;
+}
