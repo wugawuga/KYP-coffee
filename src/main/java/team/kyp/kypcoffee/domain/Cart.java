2e99a6d2c53ecc3b100f81f0acbf212171f23092
Wugawuga <dnr3655@naver.com>
2022-03-07 21:50:52 +0900

    수정 - 장바구니, 상품리스트
    변경사항 : lombok 사용해서 코드 간결히 했습니다

--- src/main/java/team/kyp/kypcoffee/domain/Cart.java
+++ src/main/java/team/kyp/kypcoffee/domain/Cart.java
@@ -1,5 +1,10 @@
 package team.kyp.kypcoffee.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Cart {
 
     private int cartNum;
@@ -12,75 +17,4 @@ public class Cart {
     private int totalPrice;
     private String productImg;
 
-    public String getProductImg() {
-        return productImg;
-    }
-
-    public void setProductImg(String productImg) {
-        this.productImg = productImg;
-    }
-
-    public int getProductPrice() {
-        return productPrice;
-    }
-
-    public void setProductPrice(int productPrice) {
-        this.productPrice = productPrice;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public int getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(int totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public int getCartNum() {
-        return cartNum;
-    }
-
-    public void setCartNum(int cartNum) {
-        this.cartNum = cartNum;
-    }
-
-    public int getMemberNum() {
-        return memberNum;
-    }
-
-    public void setMemberNum(int memberNum) {
-        this.memberNum = memberNum;
-    }
-
-    public int getProductCode() {
-        return productCode;
-    }
-
-    public void setProductCode(int productCode) {
-        this.productCode = productCode;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getCartQuantity() {
-        return cartQuantity;
-    }
-
-    public void setCartQuantity(int cartQuantity) {
-        this.cartQuantity = cartQuantity;
-    }
 }
