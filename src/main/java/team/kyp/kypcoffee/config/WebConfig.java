df2104ad0e5151568f07009c973f9eb9802fffdf
Wugawuga <dnr3655@naver.com>
2022-03-24 16:52:06 +0900

    추가 - LoginVerification.java
    세부사항 : 인터셉터 추가

--- src/main/java/team/kyp/kypcoffee/config/WebConfig.java
+++ src/main/java/team/kyp/kypcoffee/config/WebConfig.java
@@ -1,13 +1,24 @@
 package team.kyp.kypcoffee.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final HandlerInterceptor loginVerification;
+
+//    private List<String> notLoadList;
 
     //외부에 있는 상품이미지 저장폴더 경로 설정하기
     @Override
@@ -22,5 +33,15 @@ public class WebConfig implements WebMvcConfigurer {
         //registry.addResourceHandler("/productImg/**").addResourceLocations("맥북파일경로");
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(loginVerification).addPathPatterns("/mypage").excludePathPatterns("/login");
+
+        /*
+         * registry.addInterceptor(commonInterceptor) .addPathPatterns("/**") // 추가할 url
+         * 패턴 .excludePathPatterns("/user/**"); // 제외할 url 패턴
+         */
+    }
 }
 
