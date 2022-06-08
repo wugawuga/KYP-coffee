7a949a853f37fd6fe5af3a70440e63ce1a888c27
MJguri <pmy20804@gmail.com>
2022-06-08 11:12:08 +0900

    update CrawlingService

--- src/main/java/team/kyp/kypcoffee/service/CrawlingService.java
+++ src/main/java/team/kyp/kypcoffee/service/CrawlingService.java
@@ -48,8 +48,8 @@ public class CrawlingService {
         ChromeOptions options = new ChromeOptions();
 
         // 브라우저 보이지 않기
-        options.addArguments("headless");
-        options.addArguments("no-sandbox");
+        //options.addArguments("headless");
+        //options.addArguments("no-sandbox");
 
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.instagram.com/explore/tags/%EA%B3%A0%EC%9C%A4%EB%B0%95%EC%BB%A4%ED%94%BC/");
@@ -76,26 +76,30 @@ public class CrawlingService {
             driver.findElement(By.className("cmbtv")).click();
 
             //첫번째 게시글 클릭
-            driver.findElement(By.className("eLAPa")).click();
+            driver.findElement(By.className("_aagw")).click();
         }catch(NoSuchElementException e){
             System.out.println("두번째 단계 오류");
         }
 
         while(true){
             try{
-                element = driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/article/div/div[2]/div/div/div[2]/div[1]/ul/div/li/div/div/div[2]/h2/div/span/a"));
+
+                System.out.println("하이");
+
+                //element = driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/article/div/div[2]/div/div/div[2]/div[1]/ul/div/li/div/div/div[2]/h2/div/span/a"));
+                element = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/div[1]/ul/div/li/div/div/div[2]/h2/div/span/a"));
                 String userName = element.getText();
 
-                element = driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/article/div/div[2]/div/div/div[2]/div[1]/ul/div/li/div/div/div[2]/div[1]/span"));
+                element = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/div[1]/ul/div/li/div/div/div[2]/div[1]/span"));
                 String instaContent = element.getText();
                 //instaContent = instaContent.split("#")[0];
 
-                element = driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/article/div/div[1]/div/div/div[1]/img"));
+                element = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[1]/div/div/div[1]/img"));
                 String src = element.getAttribute("src");
 
-                System.out.println("userName = " + userName);
-                System.out.println("instaContent = " + instaContent);
-                System.out.println("src = " + src);
+//                System.out.println("userName = " + userName);
+//                System.out.println("instaContent = " + instaContent);
+//                System.out.println("src = " + src);
 
                 //img save
                 String fileName = fileUploadService.uploadImgUrl(src, cnt);
@@ -108,10 +112,10 @@ public class CrawlingService {
                 mapper.reviewRegi(reviewRegi);
 
                 if(cnt == 0){
-                    element = driver.findElement(By.xpath("/html/body/div[6]/div[2]/div/div/button"));
+                    element = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[1]/div/div/div/button"));
                     element.click();
                 }else{
-                    element = driver.findElement(By.xpath("/html/body/div[6]/div[2]/div/div[2]/button"));
+                    element = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[1]/div/div/div[2]/button"));
                     element.click();
                 }
                 cnt++;
